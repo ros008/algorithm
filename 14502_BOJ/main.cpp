8cf6acb83271b124b44b02d1665481e54e942dd5
sujin <sujin.k0402@gmail.com>
2020-01-29 23:39:09 +0900

    20.01.29 commit

--- 14502_BOJ/main.cpp
+++ 14502_BOJ/main.cpp
@@ -5,8 +5,8 @@
 
 using namespace std;
 
-int nr[4] = { 1,0,-1,0 };
-int nc[4] = { 0,1,0,-1 };
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
 
 int n, m;
 int board[8][8];
@@ -14,7 +14,7 @@ int boardCopy[8][8];
 bool check[64];
 int maxSafe = 0;
 
-vector<pair<int, int>> emp, virus;
+vector<pair<int, int>> virus;
 
 void countSafe() {
 	int cnt = 0;
@@ -30,22 +30,43 @@ void BFS() {
 	for (int i = 0; i < virus.size(); i++)
 		q.push({ virus[i].first, virus[i].second });
 
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
 
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+				if (boardCopy[nx][ny] == 0) {
+					boardCopy[nx][ny] = 2;
+					q.push({ nx,ny });
+				}
+			}
+		}
+	}
+
+	countSafe();
 }
 
-void makeWall(int idx, int cnt) {
+void makeWall(int cnt) {
 	if (cnt == 3) {
 		for (int i = 0; i < n; i++)
 			for (int j = 0; j < m; j++)
 				boardCopy[i][j] = board[i][j];
 		BFS();
+		return;
 	}
 
-	for (int i = idx; i < emp.size(); i++) {
-		if (check[i] == true) continue;
-		check[i] = true;
-		makeWall(i + 1, cnt + 1); //////////
-		check[i] = false;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (board[i][j] == 0) {
+				board[i][j] = 1;
+				makeWall(cnt + 1);
+				board[i][j] = 0;
+			}
+		}
 	}
 }
 
@@ -57,12 +78,12 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			cin >> board[i][j];
-			if (board[i][j] == 0) emp.push_back({ i,j });
-			else if (board[i][j] == 2) virus.push_back({ i,j });
+			if (board[i][j] == 2) 
+				virus.push_back({ i,j });
 		}
 	}
 		
-	makeWall(0, 0);
+	makeWall(0);
 
 	cout << maxSafe << "\n";
 
