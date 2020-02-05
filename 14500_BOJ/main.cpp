fcf850892d11ae1d6227f6931e5e6382fe11742f
sujin <sujin.k0402@gmail.com>
2020-02-05 22:46:49 +0900

    20.02.05 commit

--- 14500_BOJ/main.cpp
+++ 14500_BOJ/main.cpp
@@ -4,8 +4,16 @@
 
 using namespace std;
 
+int n, m;
+int map[501][501];
+
 int main() {
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			cin >> map[i][j];
 
 
 
