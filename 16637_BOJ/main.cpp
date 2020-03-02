d9a2c853bcc8a8f124eceb209094242bbf0a9785
sujin <sujin.k0402@gmail.com>
2020-03-02 17:49:07 +0900

    20.03.02 commit

--- 16637_BOJ/main.cpp
+++ 16637_BOJ/main.cpp
@@ -1,35 +1,45 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <string>
+
 #define INF 987654321
 
 using namespace std;
 
 int n, ans = -INF;
-char str[20];
+string str;
 
 int calcul(int a, int b, char op) {
 	if (op == '+')
 		return a + b;
 	else if (op == '-')
 		return a - b;
-	else if (op == '*')
+	else
 		return a * b;
-	return 0;
 }
 
-void dfs(int idx) {
+void solve(int idx, int cur) {
+	if (idx >= n) {
+		ans = max(ans, cur);
+		return;
+	}
+	char oper = (idx == 0) ? '+' : str[idx - 1];
 
+	if (idx + 2 < n) {
+		int bracket = calcul(str[idx] - '0', str[idx + 2] - '0', str[idx + 1]);
+		solve(idx + 4, calcul(cur, bracket, oper));
+	}
+	solve(idx + 2, calcul(cur, str[idx] - '0', oper));
 }
 
 int main() {
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
-	cin >> n;
-	for (int i = 1; i <= n; i++)
-		cin >> str[i];
+	cin >> n >> str;
 
-	dfs(1);
+	solve(0, 0);
+
 	cout << ans << "\n";
 
 	return 0;
