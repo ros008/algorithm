f6a1e200259fce363d6ef76e79d5ef8fa7747e50
sujin <sujin.k0402@gmail.com>
2020-03-06 20:17:06 +0900

    20.03.06 commit

--- 1613_BOJ/main.cpp
+++ 1613_BOJ/main.cpp
@@ -6,24 +6,23 @@
 
 using namespace std;
 
-const int MAX = 500 + 1;
+const int MAX = 400 + 1;
 const int INF = 987654321;
 
-int n, m;
+int n, k, s;
 int dist[MAX][MAX];
-int cnt[MAX];
 
 int main() {
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
-	cin >> n >> m;
+	cin >> n >> k;
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= n; j++)
-			dist[i][j] = INF;
+			dist[i][j] = i == j ? 0 : INF;
 
-	for (int i = 0; i < m; i++) {
+	for (int i = 0; i < k; i++) {
 		int a, b;
-		cin >> a >> b; // a<b
+		cin >> a >> b;
 		dist[a][b] = 1;
 	}
 
@@ -34,18 +33,24 @@ int main() {
 			}
 		}
 	}
-	int ans = 0;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (dist[i][j] != INF) {
-				cnt[i]++; cnt[j]++;
-				if (cnt[i] == n - 1) ans++;
-				if (cnt[j] == n - 1) ans++;
-			}
-		}
-	}
+	//for (int i = 1; i <= n; i++) {
+	//	for (int j = 1; j <= n; j++) {
+	//		cout << dist[i][j] << " ";
+	//	}
+	//	cout << "\n";
+	//}
 
-	cout << ans << "\n";
+	cin >> s;
+	for (int i = 0; i < s; i++) {
+		int a, b;
+		cin >> a >> b;
+		if (dist[a][b] == INF && dist[b][a] == INF)
+			cout << 0 << "\n";
+		else if (dist[a][b] != INF)
+			cout << -1 << "\n";
+		else
+			cout << 1 << "\n";
+	}
 
 	return 0;
 }
