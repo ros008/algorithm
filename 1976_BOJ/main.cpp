d25f7a200c5663f72b6123d16636e58e95a94b4a
sujin <sujin.k0402@gmail.com>
2020-03-10 00:09:12 +0900

    20.03.10 commit

---
+++ 1976_BOJ/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+
+	return 0;
+}
