8346dc1ccd50fcd4464ec6fc6a1fe7c45c7f81a1
sujin <sujin.k0402@gmail.com>
2020-03-18 00:32:18 +0900

    20.03.18 commit

---
+++ 4014_SWE/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+	int tc;
+	cin >> tc;
+	for (int T = 1; T <= tc; T++) {
+
+		cout << "#" << T << " " << "\n";
+	}
+
+	return 0;
+}
