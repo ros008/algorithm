aaafc8ecd179deea3f76864caefd8b18dee316f8
Kim Sujin <sujin_kim2@tmax.co.kr>
2021-09-23 17:46:53 +0900

    2021 kakao problem2 added

---
+++ Programmers/2021_kakao_blind/problem2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> orders, vector<int> course)
+{
+    vector<string> answer;
+    return answer;
+}
+
+int main()
+{
+
+    return 0;
+}
