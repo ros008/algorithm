9bcb73b8448873a5e6a1b4e86d5e0d13ebf09566
Kim Sujin <sujin_kim2@tmax.co.kr>
2021-09-23 17:42:37 +0900

    2021 kakao problem1 solved

--- Programmers/2021_kakao_blind/problem1/main.cpp
+++ Programmers/2021_kakao_blind/problem1/main.cpp
@@ -7,35 +7,63 @@ using namespace std;
 string solution(string new_id)
 {
     string answer = "";
-    int len = new_id.length() > 15 ? 15 : new_id.length();
-    for (int i = 0; i < len; i++)
+
+    // step 1
+    for (int i = 0; i < new_id.length(); i++)
     {
-        if (new_id[i] == '.')
+        if (new_id[i] >= 'A' && new_id[i] <= 'Z')
         {
-            if (i == 0 || i == len - 1 || (i != 0 && new_id[i - 1] == '.'))
-            {
-                continue;
-            }
-            answer += new_id[i];
-        }
-        else if (new_id[i] >= 'A' && new_id[i] <= 'Z')
-        {
-            answer += (new_id[i] - 'A');
-        }
-        else if ((new_id[i] >= 'a' && new_id[i] <= 'z') || (new_id[i] >= '0' && new_id[i] <= '9') || new_id[i] == '-' || new_id[i] == '_')
-        {
-            answer += new_id[i];
+            new_id[i] = tolower(new_id[i]);
         }
     }
-    if (answer.length() == 0)
-        answer += "a";
 
-    if (answer.length() <= 2)
+    // step 2
+    for (int i = 0; i < new_id.length(); i++)
     {
-        int cur_len = answer.length();
-        char last = answer[answer.length() - 1];
-        for (int i = 0; i < 3 - cur_len; i++)
-            answer += last;
+        if ((new_id[i] >= 'a' && new_id[i] <= 'z') || (new_id[i] >= '0' && new_id[i] <= '9') || new_id[i] == '-' || new_id[i] == '_' || new_id[i] == '.')
+        {
+            answer.push_back(new_id[i]);
+        }
+    }
+
+    // step 3
+    int i = 1;
+    while (i < answer.length())
+    {
+        if (answer[i - 1] == '.' && answer[i] == '.')
+        {
+            answer.erase(answer.begin() + i);
+        }
+        else
+        {
+            i++;
+        }
+    }
+
+    // step 4
+    if (answer[0] == '.')
+        answer.erase(answer.begin());
+    if (answer[answer.length() - 1] == '.')
+        answer.erase(answer.end() - 1);
+
+    // step 5
+    if (answer.length() == 0)
+        answer.push_back('a');
+
+    // step 6
+    if (answer.length() > 15)
+    {
+        answer.erase(answer.begin() + 15, answer.end());
+        if (answer[answer.length() - 1] == '.')
+        {
+            answer.erase(answer.end() - 1);
+        }
+    }
+
+    // step 7
+    while (answer.length() <= 2)
+    {
+        answer.push_back(answer[answer.length() - 1]);
     }
 
     return answer;
