53153e1609e6deb52aa55a4aa9a8cf1f1249810b
Kim Sujin <sujin_kim2@tmax.co.kr>
2021-09-27 14:04:59 +0900

    kakao 2021 problem4 added

---
+++ Programmers/2021_kakao_blind/problem4/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int s, int a, int b, vector<vector<int>> fares)
+{
+    int answer = 0;
+
+    return answer;
+}
+
+int main()
+{
+    int n = 6, s = 4, a = 6, b = 2;
+    vector<vector<int>> fares = {{4, 1, 10}, {3, 5, 24}, {5, 6, 2}, {3, 1, 41}, {5, 1, 24}, {4, 6, 50}, {2, 4, 66}, {2, 3, 22}, {1, 6, 25}};
+
+    solution(n, s, a, b, fares);
+
+    return 0;
+}
