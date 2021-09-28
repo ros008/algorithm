1f8d9af71dbeb3a2067c5c8ca7fbe491afd7df1c
Kim Sujin <sujin_kim2@tmax.co.kr>
2021-09-28 15:33:19 +0900

    dev matching 2021 problem2 added

---
+++ Programmers/2021_dev-matching_web-backend/problem2/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<int> solution(int rows, int columns, vector<vector<int>> queries)
+{
+    vector<int> answer;
+    return answer;
+}
+
+int main()
+{
+    int rows = 6;
+    int columns = 6;
+    vector<vector<int>> queries = {{2, 2, 5, 4}, {3, 3, 6, 6}, {5, 1, 6, 3}};
+
+    solution(rows, columns, queries);
+
+    return 0;
+}
