09ec7ccc5fffb18d2efb078fa6c9e05bbd5d7cc4
Kim Sujin <sujin_kim2@tmax.co.kr>
2021-09-28 13:06:31 +0900

    kakao 2021 problem6 added

---
+++ Programmers/2021_kakao_blind/problem6/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int solution(vector<vector<int>> board, int r, int c)
+{
+    int answer = 0;
+
+    return answer;
+}
+
+int main()
+{
+    vector<vector<int>> board = {{1, 0, 0, 3}, {2, 0, 0, 0}, {0, 0, 0, 2}, {3, 0, 1, 0}};
+    int r = 1, c = 0;
+
+    cout << solution(board, r, c);
+
+    return 0;
+}
