3b0f4a890d9656535cce4f68db6fa942c3cb8a7d
Kim Sujin <sujin_kim2@tmax.co.kr>
2021-09-28 13:03:44 +0900

    kakao 2021 problem5 solved

--- Programmers/2021_kakao_blind/problem5/main.cpp
+++ Programmers/2021_kakao_blind/problem5/main.cpp
@@ -4,9 +4,82 @@
 
 using namespace std;
 
+#define MAX 360001
+
+int convertTimeStringToInt(string str)
+{
+    int ret = 0;
+    string sec_str = str.substr(6, 2);
+    int sec = stoi(sec_str);
+    string min_str = str.substr(3, 2);
+    int min = stoi(min_str) * 60;
+    string hour_str = str.substr(0, 2);
+    int hour = stoi(hour_str) * 60 * 60;
+    ret = hour + min + sec;
+    return ret;
+}
+
+string convertTimeIntToString(int num)
+{
+    string ret = "";
+    int hour = num / 3600;
+    int min = (num % 3600) / 60;
+    int sec = num % 60;
+
+    if (hour <= 9)
+        ret += '0';
+    ret += to_string(hour);
+    ret += ':';
+    if (min <= 9)
+        ret += '0';
+    ret += to_string(min);
+    ret += ':';
+    if (sec <= 9)
+        ret += '0';
+    ret += to_string(sec);
+    return ret;
+}
+
 string solution(string play_time, string adv_time, vector<string> logs)
 {
     string answer = "";
+
+    int play_time_sec = convertTimeStringToInt(play_time);
+    int adv_time_sec = convertTimeStringToInt(adv_time);
+    vector<int> total_time(360001, 0);
+
+    for (int i = 0; i < logs.size(); i++)
+    {
+        int start_time = convertTimeStringToInt(logs[i].substr(0, 8));
+        int end_time = convertTimeStringToInt(logs[i].substr(9, 8));
+        cout << start_time << " " << end_time << "\n";
+        for (int j = start_time; j < end_time; j++)
+        {
+            total_time[j]++;
+        }
+    }
+
+    long long max_play_cnt = 0;
+    long long cur_play_cnt = 0;
+    int max_time_start = 0;
+
+    for (int i = 0; i < adv_time_sec; i++)
+        max_play_cnt += total_time[i];
+
+    cur_play_cnt = max_play_cnt;
+
+    for (int i = adv_time_sec; i < play_time_sec; i++)
+    {
+        cout << total_time[i] << " " << total_time[i - adv_time_sec];
+        cur_play_cnt += (long long)(total_time[i] - total_time[i - adv_time_sec]);
+        cout << cur_play_cnt;
+        if (cur_play_cnt > max_play_cnt)
+        {
+            max_play_cnt = cur_play_cnt;
+            max_time_start = i - adv_time_sec + 1;
+        }
+    }
+    answer = convertTimeIntToString(max_time_start);
 
     return answer;
 }
