92d4c41cd11ab9df4664a46d18658e8438318bef
sujin <sujin.k0402@gmail.com>
2021-12-03 00:16:36 +0900

    boj 15591 solved

---
+++ BOJ/17780_BOJ/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int N, K;
+
+int main()
+{
+    cin >> N >> K;
+    for (int i = 0; i < N; i++)
+    {
+    }
+    for (int i = 0; i < K; i++)
+    {
+    }
+
+    return 0;
+}
