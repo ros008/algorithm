d752dffa70c323f662e640a87d6faebbf25c6650
sujin <sujin.k0402@gmail.com>
2025-01-07 14:32:49 +0900

    feat: js leetcode best time to buy and sell stock 2 solved

--- JS/leetcode/leetcode_best_time_to_buy_and_sell_stock.js
+++ JS/leetcode/leetcode_best_time_to_buy_and_sell_stock.js
@@ -4,10 +4,12 @@
  */
 var maxProfit = function (prices) {
   let ans = 0;
+  let min = prices[0];
+
   for (let i = 1; i < prices.length; i++) {
-    if (prices[i] > prices[i - 1]) {
-      ans += prices[i] - prices[i - 1];
-    }
+    min = Math.min(min, prices[i]);
+    ans = Math.max(ans, prices[i] - min);
   }
+
   return ans;
 };
