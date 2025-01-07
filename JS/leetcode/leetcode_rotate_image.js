6eb3d7f21b755d341c526221990fac6e6a49745c
sujin <sujin.k0402@gmail.com>
2025-01-07 22:51:20 +0900

    feat: js leetcode rotate image solved

---
+++ JS/leetcode/leetcode_rotate_image.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < i; j++) {
+      let tmp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = tmp;
+    }
+  }
+  console.log(matrix);
+
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i].reverse();
+  }
+};
